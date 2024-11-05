660bbd16af10d7801bf2ecfc7b5338d57a1dbe65
minhlc1108 <minh.lcm.2003@gmail.com>
2024-11-05 13:19:16 +0700

    fix import in index.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { ConfigProvider } from "antd";
 import { createRoot } from "react-dom/client";
 import { App as AntdApp } from "antd";
-import EscapeAntd from "./components/EscapeAntd/index.jsx";
+import EscapeAntd from "~/components/EscapeAntd";
 import App from "./App.jsx";
 import "./index.css";
 
