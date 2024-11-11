c2c52f2f1fcc53b679ff97c92495ac0f6480e38d
minhlc1108 <minh.lcm.2003@gmail.com>
2024-11-11 23:56:31 +0700

    fetch all tour

--- src/pages/Admin/Statistic.jsx
+++ src/pages/Admin/Statistic.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Button, Card, Col, Flex, Row, Space, Typography } from "antd";
+import React from "react";
 
 function Statistic() {
   return (
-    <div>Statistic</div>
-  )
+    <>
+      <Row gutter={[16, 16]}>
+        <Col xs={24} lg={9}>
+          <Card>
+            Có x đơn hàng chưa xử lý <Button>Đi đến xử lý</Button>
+          </Card>
+        </Col>
+        <Col xs={8} lg={5}>
+          <Card>
+            <Flex vertical align="center" justify="center">
+              <Space style={{ fontSize: 30, fontWeight: "bold" }}>Tour</Space>
+              <Space>
+                <Space>19</Space> +
+              </Space>
+            </Flex>
+          </Card>
+        </Col>
+        <Col xs={8} lg={5}>
+          <Card>Khách hàng</Card>
+        </Col>
+        <Col xs={8} lg={5}>
+          <Card>Đơn đặt</Card>
+        </Col>
+
+        <Col xs={24}>
+          <Card>Thống kê doanh thu</Card>
+        </Col>
+
+        <Col xs={24}>
+          <Card>Thống kê tour đặt nhiều</Card>
+        </Col>
+
+        <Col xs={24}>
+          <Card>Khách hàng đặt nhiều</Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
 
-export default Statistic
+export default Statistic;
